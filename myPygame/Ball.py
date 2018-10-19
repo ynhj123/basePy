dd75894f6f8a38c9430249c037af4887eb366aec
杨牛浩江 <542182536@qq.com>
2018-10-19 14:52:55 +0800

    随便写写

--- myPygame/Ball.py
+++ myPygame/Ball.py
@@ -1,3 +1,7 @@
+import time
+
+from myPygame.Contants import SPEED
+
 
 class Ball:
     def __init__(self, position, radius) -> None:
@@ -9,5 +13,28 @@ class Ball:
 
     def move_y(self, y):
         self.postition.y += y
+
+    def move_to(self, postition):
+        self.postition = postition
+
+    def move_x_speed(self, x, speed):
+        if (self.postition.x <= x):
+            while self.postition.x >= x:
+                time.sleep(SPEED)
+                self.postition.x += speed
+        else:
+            while self.postition.x <= x:
+                time.sleep(SPEED)
+                self.postition.x -= speed
+
+    def move_y_speed(self, y, speed):
+        if (self.postition.y <= y):
+            while self.postition.y >= y:
+                time.sleep(SPEED)
+                self.postition.y += speed
+        else:
+            while self.postition.y <= y:
+                time.sleep(SPEED)
+                self.postition.y -= speed
 
 
--- myPygame/Position.py
+++ myPygame/Position.py
@@ -1,4 +1,8 @@
 class Position:
-    def __init__(self,x,y) -> None:
+    def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
+
+
+    def __eq__(self, position):
+        return self.x == position.x and self.y == position.y
